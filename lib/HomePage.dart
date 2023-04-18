ac0b6269b4a6e3590ab6c688b6cb60231e945cb7
linoy193 <58636678+linoy193@users.noreply.github.com>
2023-04-19 02:01:51 +0300

    scan barcod func

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -170,3 +170,20 @@ class _HomePageState extends State<HomePage> {
     } on PlatformException {
       barcodeScanRes = 'Failed to get platform version.';
     }
+    
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _scanBarcode = barcodeScanRes;
+    });
+  }
+
+  scan_barcode() {
+    FlutterBarcodeScanner.scanBarcode(
+            "#000000", "Cancel", true, ScanMode.BARCODE)
+        .then((value) => setState(() => _data = value));
+  }
+}
